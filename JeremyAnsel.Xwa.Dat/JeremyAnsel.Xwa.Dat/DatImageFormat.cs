a75344e10a5a2e51385df2f8564043fa88ad085e
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2015-10-16 16:22:56 +0200

    [skip ci] Update ImageFormat doc

--- JeremyAnsel.Xwa.Dat/JeremyAnsel.Xwa.Dat/DatImageFormat.cs
+++ JeremyAnsel.Xwa.Dat/JeremyAnsel.Xwa.Dat/DatImageFormat.cs
@@ -10,12 +10,28 @@ namespace JeremyAnsel.Xwa.Dat
     [SuppressMessage("Microsoft.Design", "CA1008:EnumsShouldHaveZeroValue", Justification = "Reviewed")]
     public enum DatImageFormat
     {
+        /// <summary>
+        /// Format: 8-bit indexed colors and 1-bit alpha, rle compressed.
+        /// Use: concourse or in-flight.
+        /// </summary>
         Format7 = 7,
 
+        /// <summary>
+        /// Format: 8-bit indexed colors and 8-bit alpha, rle compressed.
+        /// Use: concourse.
+        /// </summary>
         Format23 = 23,
 
+        /// <summary>
+        /// Format: 8-bit indexed colors and 8-bit alpha.
+        /// Use: in-flight.
+        /// </summary>
         Format24 = 24,
 
+        /// <summary>
+        /// Format: 32-bit ARGB.
+        /// Use: none.
+        /// </summary>
         Format25 = 25,
     }
 }
