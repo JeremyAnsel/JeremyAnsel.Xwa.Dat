95082e92568f21b52da3c27bf84cd3b9843f4f3b
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2023-03-26 14:07:03 +0200

    Add stream methods

--- JeremyAnsel.Xwa.Dat/JeremyAnsel.Xwa.Dat/DatFile.cs
+++ JeremyAnsel.Xwa.Dat/JeremyAnsel.Xwa.Dat/DatFile.cs
@@ -44,123 +44,113 @@ namespace JeremyAnsel.Xwa.Dat
             }
         }
 
-        [SuppressMessage("Style", "IDE0017:Simplifier l'initialisation des objets", Justification = "Reviewed.")]
         public static DatFile FromFile(string fileName)
         {
-            var dat = new DatFile
+            using var filestream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+
+            DatFile dat = FromStream(filestream);
+            dat.FileName = fileName;
+
+            return dat;
+        }
+
+        [SuppressMessage("Style", "IDE0017:Simplifier l'initialisation des objets", Justification = "Reviewed.")]
+        public static DatFile FromStream(Stream stream)
+        {
+            var dat = new DatFile();
+
+            using var file = new BinaryReader(stream, Encoding.ASCII, true);
+
+            long signature = file.ReadInt64();
+
+            if (signature != DatFile.Signature)
             {
-                FileName = fileName
-            };
+                throw new InvalidDataException();
+            }
 
-            FileStream filestream = null;
+            short groupFormat = file.ReadInt16();
+            short count = file.ReadInt16();
 
-            try
+            file.ReadInt16(); // images count
+            file.ReadInt32(); // images length
+
+            if (groupFormat != 0)
             {
-                filestream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+                file.BaseStream.Position += 12;
+            }
 
-                using (BinaryReader file = new BinaryReader(filestream, Encoding.ASCII))
+            file.BaseStream.Position += 4;
+
+            for (int i = 0; i < count; i++)
+            {
+                var group = new DatGroup();
+
+                group.GroupId = file.ReadInt16();
+                int imageCount = file.ReadInt16();
+                file.ReadInt32(); // length
+
+                if (groupFormat != 0)
                 {
-                    filestream = null;
+                    file.ReadInt32(); // colors count
+                    int m0C = file.ReadInt32();
+                    int m10 = file.ReadInt32();
 
-                    long signature = file.ReadInt64();
+                    if (m0C != 0 || m10 != 0)
+                    {
+                        throw new InvalidDataException();
+                    }
+                }
 
-                    if (signature != DatFile.Signature)
+                file.ReadInt32(); // offset
+
+                ((List<DatImage>)group.Images).Capacity = imageCount;
+
+                dat.Groups.Add(group);
+            }
+
+            foreach (var group in dat.Groups)
+            {
+                int imageCount = ((List<DatImage>)group.Images).Capacity;
+
+                for (int i = 0; i < imageCount; i++)
+                {
+                    var image = new DatImage();
+
+                    image.Format = (DatImageFormat)file.ReadInt16();
+                    image.Width = file.ReadInt16();
+                    image.Height = file.ReadInt16();
+                    file.ReadUInt16(); // color key
+                    file.ReadInt16(); // colors count
+                    image.GroupId = file.ReadInt16();
+                    image.ImageId = file.ReadInt16();
+
+                    int dataLength = file.ReadInt32();
+
+                    if (dataLength < 0x2C)
                     {
                         throw new InvalidDataException();
                     }
 
-                    short groupFormat = file.ReadInt16();
-                    short count = file.ReadInt16();
+                    file.BaseStream.Position += 0x18;
+                    image.OffsetX = file.ReadInt32();
+                    image.OffsetY = file.ReadInt32();
+                    file.BaseStream.Position += 0x08;
+                    image.ColorsCount = (short)file.ReadInt32();
 
-                    file.ReadInt16(); // images count
-                    file.ReadInt32(); // images length
-
-                    if (groupFormat != 0)
+                    if (image.Format == DatImageFormat.Format25)
                     {
-                        file.BaseStream.Position += 12;
-                    }
-
-                    file.BaseStream.Position += 4;
-
-                    for (int i = 0; i < count; i++)
-                    {
-                        var group = new DatGroup();
-
-                        group.GroupId = file.ReadInt16();
-                        int imageCount = file.ReadInt16();
-                        file.ReadInt32(); // length
-
-                        if (groupFormat != 0)
+                        switch (image.ColorsCount)
                         {
-                            file.ReadInt32(); // colors count
-                            int m0C = file.ReadInt32();
-                            int m10 = file.ReadInt32();
-
-                            if (m0C != 0 || m10 != 0)
-                            {
-                                throw new InvalidDataException();
-                            }
-                        }
-
-                        file.ReadInt32(); // offset
-
-                        ((List<DatImage>)group.Images).Capacity = imageCount;
-
-                        dat.Groups.Add(group);
-                    }
-
-                    foreach (var group in dat.Groups)
-                    {
-                        int imageCount = ((List<DatImage>)group.Images).Capacity;
-
-                        for (int i = 0; i < imageCount; i++)
-                        {
-                            var image = new DatImage();
-
-                            image.Format = (DatImageFormat)file.ReadInt16();
-                            image.Width = file.ReadInt16();
-                            image.Height = file.ReadInt16();
-                            file.ReadUInt16(); // color key
-                            file.ReadInt16(); // colors count
-                            image.GroupId = file.ReadInt16();
-                            image.ImageId = file.ReadInt16();
-
-                            int dataLength = file.ReadInt32();
-
-                            if (dataLength < 0x2C)
-                            {
-                                throw new InvalidDataException();
-                            }
-
-                            file.BaseStream.Position += 0x18;
-                            image.OffsetX = file.ReadInt32();
-                            image.OffsetY = file.ReadInt32();
-                            file.BaseStream.Position += 0x08;
-                            image.ColorsCount = (short)file.ReadInt32();
-
-                            if (image.Format == DatImageFormat.Format25)
-                            {
-                                switch (image.ColorsCount)
-                                {
-                                    case 1:
-                                        image.Format = DatImageFormat.Format25C;
-                                        image.ColorsCount = 0;
-                                        break;
-                                }
-                            }
-
-                            image.rawData = file.ReadBytes(dataLength - 0x2C);
-
-                            group.Images.Add(image);
+                            case 1:
+                                image.Format = DatImageFormat.Format25C;
+                                image.ColorsCount = 0;
+                                break;
                         }
                     }
-                }
-            }
-            finally
-            {
-                if (filestream != null)
-                {
-                    filestream.Dispose();
+
+                    image.rawData = file.ReadBytes(dataLength - 0x2C);
+
+                    group.Images.Add(image);
                 }
             }
 
@@ -169,99 +159,88 @@ namespace JeremyAnsel.Xwa.Dat
 
         public void Save(string fileName)
         {
-            FileStream filestream = null;
+            using var filestream = new FileStream(fileName, FileMode.Create, FileAccess.Write);
 
-            try
+            this.Save(filestream);
+            this.FileName = fileName;
+        }
+
+        public void Save(Stream stream)
+        {
+            using var file = new BinaryWriter(stream, Encoding.ASCII, true);
+
+            file.Write(DatFile.Signature);
+            file.Write((short)1);
+
+            file.Write((short)this.Groups.Count);
+            file.Write((short)this.Groups.Sum(t => t.Images.Count));
+            file.Write(this.Groups.Sum(t => t.Images.Sum(i => 0x12 + 0x2C + (i.rawData == null ? 0 : i.rawData.Length))));
+            file.Write(this.Groups.Sum(t => t.Images.Sum(i => i.ColorsCount)));
+            file.Write(0);
+            file.Write(0);
+            file.Write(this.Groups.Count * 0x18);
+
+            int groupOffset = 0;
+
+            foreach (var group in this.Groups)
             {
-                filestream = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+                int groupLength = group.Images.Sum(i => 0x12 + 0x2C + (i.rawData == null ? 0 : i.rawData.Length));
 
-                using (BinaryWriter file = new BinaryWriter(filestream, Encoding.ASCII))
-                {
-                    filestream = null;
+                file.Write(group.GroupId);
+                file.Write((short)group.Images.Count);
+                file.Write(groupLength);
+                file.Write(group.Images.Sum(i => i.ColorsCount));
+                file.Write(0);
+                file.Write(0);
+                file.Write(groupOffset);
 
-                    file.Write(DatFile.Signature);
-                    file.Write((short)1);
-
-                    file.Write((short)this.Groups.Count);
-                    file.Write((short)this.Groups.Sum(t => t.Images.Count));
-                    file.Write(this.Groups.Sum(t => t.Images.Sum(i => 0x12 + 0x2C + (i.rawData == null ? 0 : i.rawData.Length))));
-                    file.Write(this.Groups.Sum(t => t.Images.Sum(i => i.ColorsCount)));
-                    file.Write(0);
-                    file.Write(0);
-                    file.Write(this.Groups.Count * 0x18);
-
-                    int groupOffset = 0;
-
-                    foreach (var group in this.Groups)
-                    {
-                        int groupLength = group.Images.Sum(i => 0x12 + 0x2C + (i.rawData == null ? 0 : i.rawData.Length));
-
-                        file.Write(group.GroupId);
-                        file.Write((short)group.Images.Count);
-                        file.Write(groupLength);
-                        file.Write(group.Images.Sum(i => i.ColorsCount));
-                        file.Write(0);
-                        file.Write(0);
-                        file.Write(groupOffset);
-
-                        groupOffset += groupLength;
-                    }
-
-                    foreach (var image in this.Groups.SelectMany(t => t.Images))
-                    {
-                        int rawDataLength = image.rawData == null ? 0 : image.rawData.Length;
-
-                        DatImageFormat format;
-                        short colorsCount;
-
-                        switch (image.Format)
-                        {
-                            case DatImageFormat.Format25C:
-                                format = DatImageFormat.Format25;
-                                colorsCount = 1;
-                                break;
-
-                            default:
-                                format = image.Format;
-                                colorsCount = image.ColorsCount;
-                                break;
-                        }
-
-                        file.Write((short)format);
-                        file.Write(image.Width);
-                        file.Write(image.Height);
-                        file.Write((ushort)0);
-                        file.Write((short)0);
-                        file.Write(image.GroupId);
-                        file.Write(image.ImageId);
-                        file.Write(0x2C + rawDataLength);
-
-                        file.Write(0x2C + rawDataLength);
-                        file.Write(0x2C);
-                        file.Write(0x2C + image.ColorsCount * 3);
-                        file.Write(0x2C + rawDataLength);
-                        file.Write((int)image.Width);
-                        file.Write((int)image.Height);
-                        file.Write(image.OffsetX);
-                        file.Write(image.OffsetY);
-                        file.Write((int)format);
-                        file.Write(0x18);
-                        file.Write((int)colorsCount);
-
-                        if (image.rawData != null)
-                        {
-                            file.Write(image.rawData);
-                        }
-                    }
-
-                    this.FileName = fileName;
-                }
+                groupOffset += groupLength;
             }
-            finally
+
+            foreach (var image in this.Groups.SelectMany(t => t.Images))
             {
-                if (filestream != null)
+                int rawDataLength = image.rawData == null ? 0 : image.rawData.Length;
+
+                DatImageFormat format;
+                short colorsCount;
+
+                switch (image.Format)
                 {
-                    filestream.Dispose();
+                    case DatImageFormat.Format25C:
+                        format = DatImageFormat.Format25;
+                        colorsCount = 1;
+                        break;
+
+                    default:
+                        format = image.Format;
+                        colorsCount = image.ColorsCount;
+                        break;
+                }
+
+                file.Write((short)format);
+                file.Write(image.Width);
+                file.Write(image.Height);
+                file.Write((ushort)0);
+                file.Write((short)0);
+                file.Write(image.GroupId);
+                file.Write(image.ImageId);
+                file.Write(0x2C + rawDataLength);
+
+                file.Write(0x2C + rawDataLength);
+                file.Write(0x2C);
+                file.Write(0x2C + image.ColorsCount * 3);
+                file.Write(0x2C + rawDataLength);
+                file.Write((int)image.Width);
+                file.Write((int)image.Height);
+                file.Write(image.OffsetX);
+                file.Write(image.OffsetY);
+                file.Write((int)format);
+                file.Write(0x18);
+                file.Write((int)colorsCount);
+
+                if (image.rawData != null)
+                {
+                    file.Write(image.rawData);
                 }
             }
         }
